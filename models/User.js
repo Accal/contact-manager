6483ccc286d931857343647bd38aff2ad791bda7
Ammar Rahic <ammarrahic@live.com>
2019-10-13 11:44:00 +0200

    Add jwt on sucessful register

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  name: {
+  email: {
     type: String,
     required: true,
     unique: true
@@ -20,4 +20,4 @@ const UserSchema = mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('user', UserSchema) ;
+module.exports = mongoose.model('user', UserSchema);
